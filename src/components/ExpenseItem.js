76b16edc49214b1ac7a2fd7a9fe9d53deb3145e9
Aman <bishtaman08@gmail.com>
2023-03-03 22:21:57 +0530

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,30 @@
+
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  const [amount, setAmount] = useState(props.amount);
+  console.log('ExpenseItem evaluated by React');
+  
+  const clickHandler = () => {
+    setAmount('100$');
+    //console.log(title);
+  };
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
