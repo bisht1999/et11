7b9fabe99e8e5720baa8a32a9982753aec84631d
Aman <bishtaman08@gmail.com>
2023-03-10 04:29:57 +0530

    another commit

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -17,4 +17,3 @@ const ExpenseDate = (props) => {
 };
 
 export default ExpenseDate;
-// as;dlkfa'sdklfj
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,19 @@
 
-import React, { useState } from 'react';
+
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
-import Card from '../../UI/Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // function clickHandler() {}
-  const [amount, setAmount] = useState(props.amount);
-  console.log('ExpenseItem evaluated by React');
-  
-  const clickHandler = () => {
-    setAmount('100$');
-    //console.log(title);
-  };
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
