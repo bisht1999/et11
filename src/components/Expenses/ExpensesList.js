9259ee44d1ecc8da06b478f3284ed363e94db5bd
Aman <bishtaman08@gmail.com>
2023-03-10 05:31:51 +0530

    next commit

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+  if (props.items.length === 1) {
+    return <h2 className='expenses-list__fallback'> Only single expense. Add more </h2>;
+  }
+
+  return (
+    <ul className='expenses-list'>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
